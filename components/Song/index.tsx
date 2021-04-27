cb827f341908915707d5d7a89274d7d2734d550d
ignapbian <ignacioga62@gmail.com>
2021-04-27 13:30:20 +0200

    Video Screen Datails

--- components/Song/index.tsx
+++ components/Song/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import { View,Text,Image } from 'react-native';
 import styles from './styles';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { getCustomTabsSupportingBrowsersAsync } from 'expo-web-browser';
+import { useNavigation } from '@react-navigation/native';
 
 export type SongProps={
     data:any;
 }
 
 const Song = (props:SongProps) =>{
+    const navigation = useNavigation();
+    const goToInfo = ()=>{
+        navigation.navigate('VideoInfoScreen',{data:props.data.item})
+    }
         return (
-            <View style={styles.songContainer}>
+            <TouchableOpacity onPress={goToInfo} style={styles.songContainer}>
                  <Image style={styles.imageSong} source={{uri: props.data.item.image_url}}/>
                  <View style={styles.songTextContainer}>
                     <Text style={styles.title}>{props.data.item.title}</Text>
                     <Text style={styles.artist}>{props.data.item.artist}</Text>
                  </View>
-            </View>
+            </TouchableOpacity>
         )
 }
 export default Song;
--- screens/VideoInfoScreen.tsx
+++ screens/VideoInfoScreen.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View,StyleSheet } from 'react-native'
+import { View,StyleSheet, Image,Text } from 'react-native'
 import { useRoute } from '@react-navigation/native';
 
 const VideoInfoScreen = () =>{
@@ -8,7 +8,12 @@ const VideoInfoScreen = () =>{
     console.log(props);
         return (
             <View style={style.container}>
-                
+                <Image style={style.imageSong} source={{uri: props.image_url}}/>
+                    <View style={style.songTextContainer}>
+                        <Text style={style.title}>{props.title}</Text>
+                        <Text style={style.artist}>Artist: {props.artist}</Text>
+                        <Text style={style.release}>Release: {props.release_year}</Text>
+                    </View>
             </View>
         )
 }
@@ -16,8 +21,43 @@ export default VideoInfoScreen;
 
 const style = StyleSheet.create({
     container:{
-
+        width:'100%',
+        height:292,
+        justifyContent:'center',
+        alignItems:'center',
     },
+    imageSong:{
+        width:'100%',
+        height:292,
+        borderColor:'#14213D',
+        borderWidth:4,
+        marginBottom:10,
+        marginTop:500
+    },
+    songTextContainer:{
+        justifyContent:'space-between',
+        flexDirection:'column',
+        alignItems:'center',
+        padding:10
+        
+    },
+    title:{
+        color:'#FFFFFF',
+        fontSize:20,
+        fontWeight:'bold',
+        marginBottom:20
+    },
+    artist:{
+        color:'#E5E5E5',
+        fontSize:20,
+        marginBottom:20
+    },
+    release:{
+        color:'#FCA311',
+        fontSize:20,
+    }
+
+
 
 
 })
